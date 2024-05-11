c88c2bc204e7513e9d61dea31d47ecb71fba1201
uxiun <teppeiito0905@gmail.com>
2024-05-11 23:20:40 +0900

    .

--- src/App.fs
+++ src/App.fs
@@ -15,23 +15,12 @@ let rec update (msg: Msg) (state: State) : State =
   match msg.Counter with
   | Some counter -> update { msg with Counter = None } { state with Counter = Counter.update counter state.Counter }
   | None ->
+
     match msg.Input with
     | Some m -> update { msg with Input = None } { state with Input = Input.update m state.Input }
-    | None -> state
+    | None ->
 
-// let update (msg: Msg) (state: State) : State =
-//     match msg.Counter, msg.Input with
-//     | Some c, Some i ->
-//         { state with
-//             Counter = Counter.update c state.Counter
-//             Input = Input.update i state.Input }
-//     | Some c, None ->
-//         { state with
-//             Counter = Counter.update c state.Counter }
-//     | None, Some i ->
-//         { state with
-//             Input = Input.update i state.Input }
-//     | _ -> state
+      state
 
 let render (state: State) (dispatch: Msg -> unit) : ReactElement =
   Html.div [
